e96573cb5e310b70e782eb17dd74240a4b67d452
tinh <thuongtinh2000@gmail.com>
2021-10-14 10:44:37 +0700

    commit

---
+++ _13_transaction/thuc_hanh/thuc_hanh_1/src/main/java/model/repository/IUserRepository.java
@@ -0,0 +1,27 @@
+package model.repository;
+
+import model.bean.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserRepository {
+    void insertUser(User user) throws SQLException;
+
+    User selectUser(int id);
+
+    List<User> selectAllUsers();
+
+    boolean deleteUser(int id) throws SQLException;
+
+    boolean updateUser(User user) throws SQLException;
+
+    List<User> search(String country);
+
+    List<User> listAsc();
+
+    User getUserById(int id);
+
+    void insertUserStore(User user) throws SQLException;
+
+}
